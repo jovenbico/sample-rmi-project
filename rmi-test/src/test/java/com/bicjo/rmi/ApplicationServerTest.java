b418d1d7d51e49028b6a5dc91a5a79865ec9e4df
Joven Frankie Bico <jfbico@xmnt.net>
2016-07-14 19:28:59 +0800

    test

---
+++ rmi-test/src/test/java/com/bicjo/rmi/ApplicationServerTest.java
@@ -0,0 +1,35 @@
+package com.bicjo.rmi;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.bicjo.rmi.module.AddressService;
+
+public class ApplicationServerTest extends Assert {
+
+	private final Logger LOG = LogManager.getLogger(getClass());
+
+	@Test
+	public void lookup_test() {
+
+		AddressService addressService = null;
+
+		try {
+			addressService = (AddressService) Naming.lookup("rmi://localhost:1066/addressService");
+		} catch (MalformedURLException | RemoteException | NotBoundException e) {
+			LOG.error(e.getMessage(), e);
+		}
+
+		LOG.debug("AddressService {}", addressService);
+		assertNotNull(addressService);
+
+	}
+
+}
